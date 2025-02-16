72d5dd5165b319c00dbfc6ad08df63d2337d82a7
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-02-16 18:47:14 -0300

    chore: add twmerge and configure tailwind css with oklch colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,19 +9,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--color-background)",
-        foreground: "var(--color-foreground)",
-        ["surface-darker"]: "var(--color-surface-darker)",
-        ["surface-accent"]: "var(--color-surface-accent)",
-        ["surface-lighter"]: "var(--color-surface-lighter)",
-        ["primary-900"]: "var(--color-primary-900)",
-        ["primary-500"]: "var(--color-primary-500)",
-        ["secondary-500"]: "var(--color-secondary)",
-        ["tertiary-500"]: "var(--color-tertiary)",
-        ["on-primary"]: "var(--color-on-primary)",
-        ["on-secondary"]: "var(--color-on-secondary)",
-        ["on-tertiary"]: "var(--color-on-tertiary)",
-        ["on-surface"]: "var(--color-on-surface)",
       },
     },
   },
