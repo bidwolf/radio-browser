f3cc24cdd705ce2bbb9aa8dd9578de81d264fb5f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-02-18 01:49:32 -0300

    feat: add FavoritesTable and Stations components with filtering functionality and loading states

---
+++ src/app/favorites/components/FavoritesTable/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React, { use } from "react";
+import { FavoritesContext } from "./FavoritesContext"
+import { StationItem, StationItemSkeleton } from "@/app/stations/components/StationItem";
+import TransmissionIcon from "@/components/icons/transmision";
+import { FilterType } from "@/types";
+
+type FavoriteStationsProps = {
+  filter: FilterType
+  value: string
+  offset: number
+}
+export const FavoriteStations: React.FC<FavoriteStationsProps> = ({ filter, value, offset }) => {
+  const { favorites, isLoading } = use(FavoritesContext)
+  const filteredFavorites = favorites.filter(station => {
+    if (filter === FilterType.COUNTRY) {
+      return station.country === value
+    } else if (filter === FilterType.LANGUAGE) {
+      return station.language === value
+    } else {
+      return station.name.toLowerCase().includes(value.toLowerCase())
+    }
+  }).slice(offset, offset + 10)
+  return (
+
+    <div className="@container w-full p-4 mb-24">
+      <div className="flex flex-col gap-4 @xl:grid @xl:grid-cols-2 @4xl:grid-cols-3 @7xl:grid-cols-4 @8xl:grid-cols-5 ">
+        {
+          isLoading ? (
+            Array.from({ length: 10 }).map((_, i) => (
+              <StationItemSkeleton key={i} />
+            ))
+          ) :
+            filteredFavorites.length === 0 ? (
+              <div className="flex flex-col gap-2 p-4 rounded-md col-span-full">
+                <TransmissionIcon className="w-12 h-12 mx-auto fill-on-surface" />
+                <h3 className="text-sm text-center text-on-surface font-semibold font-body">Nenhuma estação encontrada no momento</h3>
+              </div>
+            ) :
+              filteredFavorites.map(station => (
+                <StationItem key={station.stationuuid} {...station} />
+              ))
+        }
+      </div>
+    </div>
+  )
+}
---
+++ src/app/stations/components/Stations.tsx
@@ -0,0 +1,57 @@
+import { FilterType, Station } from "@/types";
+import { StationItem } from "./StationItem";
+import TransmissionIcon from "@/components/icons/transmision";
+
+const fetchStationData = async (query: string, offset: number = 0, filterType: FilterType | null) => {
+  const base = 'http://de1.api.radio-browser.info/json/stations'
+  let url = ''
+  switch (filterType) {
+    case FilterType.NAME:
+      url = `${base}/byname/${query}?limit=10&offset=${offset}`
+      break
+    case FilterType.COUNTRY:
+      url = `${base}/bycountry/${query}?limit=10&offset=${offset}`
+      break
+    case FilterType.LANGUAGE:
+      url = `${base}/bylanguage/${query}?limit=10&offset=${offset}`
+      break
+    default:
+      url = `${base}?limit=10&offset=${offset}`
+  }
+  const response = await fetch(url, {
+    cache: 'force-cache'
+  })
+  if (!response.ok) {
+    throw new Error('Failed to fetch station data')
+  }
+  return response.json() as Promise<Station[]>
+}
+export default async function Stations({
+  query,
+  offset,
+  filterType
+}: {
+  query: string,
+  offset?: number,
+  filterType: FilterType | null
+}) {
+  const stations = await fetchStationData(query, offset, filterType)
+
+  return (
+    <div className="@container w-full p-4 mb-24">
+      <div className="flex flex-col gap-4 @xl:grid @xl:grid-cols-2 @4xl:grid-cols-3 @7xl:grid-cols-4 @8xl:grid-cols-5">
+        {
+          stations.length === 0 ? (
+            <div className="flex flex-col gap-2 p-4 rounded-md col-span-full">
+              <TransmissionIcon className="w-12 h-12 mx-auto fill-on-surface" />
+              <h3 className="text-sm text-center text-on-surface font-semibold font-body">Nenhuma estação encontrada no momento</h3>
+            </div>
+          ) :
+            stations.map(station => (
+              <StationItem key={station.stationuuid} {...station} />
+            ))
+        }
+      </div>
+    </div>
+  )
+}
